d4b1ffb85374e0626185e4deccfde2bd3ab3e97e
Aubrey Badger <akbadger@gmail.com>
2017-02-28 22:05:30 -0500

    finished structure, continued styling etsy page

--- js/global.js
+++ js/global.js
@@ -83,6 +83,34 @@ createCard({
     price: '$37.00'
 });
 
+createCard({
+    src: 'https://unsplash.it/400?image=985',
+    caption: 'Something Else',
+    shop: 'This Place',
+    price: '$37.00'
+});
+
+createCard({
+    src: 'https://unsplash.it/400?image=985',
+    caption: 'Something Else',
+    shop: 'This Place',
+    price: '$37.00'
+});
+
+createCard({
+    src: 'https://unsplash.it/400?image=985',
+    caption: 'Something Else',
+    shop: 'This Place',
+    price: '$37.00'
+});
+
+createCard({
+    src: 'https://unsplash.it/400?image=985',
+    caption: 'Something Else',
+    shop: 'This Place',
+    price: '$37.00'
+});
+
 function createCard(image) {
     var card = `<div class="card">
         <img src=${image.src} alt="image"/>
